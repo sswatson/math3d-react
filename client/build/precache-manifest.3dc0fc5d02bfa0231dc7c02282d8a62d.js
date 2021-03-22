df5b3500905aa41820b77937cfa6a73b8c3f6d71
Samuel S. Watson <samuel.s.watson@gmail.com>
2021-03-22 17:40:14 -0400

    Add build artifacts

---
+++ client/build/precache-manifest.3dc0fc5d02bfa0231dc7c02282d8a62d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5e273999f2acb67cf998937c369d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebe6f6bf9828aec4dd84",
+    "url": "/static/css/2.6179de50.chunk.css"
+  },
+  {
+    "revision": "590ba66b4fa97a195a77",
+    "url": "/static/css/main.9d74ee7a.chunk.css"
+  },
+  {
+    "revision": "ebe6f6bf9828aec4dd84",
+    "url": "/static/js/2.dc13f81e.chunk.js"
+  },
+  {
+    "revision": "d5d1bcf329b509967edeec7d9e9287ef",
+    "url": "/static/js/2.dc13f81e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590ba66b4fa97a195a77",
+    "url": "/static/js/main.1a6b3319.chunk.js"
+  },
+  {
+    "revision": "8be0a1c73efef326921b",
+    "url": "/static/js/runtime-main.884e40fd.js"
+  }
+]);
